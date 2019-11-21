ed23a3400168e85fac983d79d7b3ffaab7d37f05
amzflood <aflood@mymail.mines.edu>
2019-11-21 10:44:16 -0700

    updated

--- LoadFile.py
+++ LoadFile.py
@@ -1,8 +1,6 @@
 import string
 
 def LoadFile(file):
-      original_word_list = []
-      temp_list = []
       with open (file) as f:
             lines = f.read().splitlines()
       return lines
