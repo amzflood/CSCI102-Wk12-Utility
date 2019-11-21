9953f475fecdb2916c52c489c21064b81c5738e5
amzflood <aflood@mymail.mines.edu>
2019-11-21 11:13:50 -0700

    updated

--- UpdateString.py
+++ UpdateString.py
@@ -1,1 +1,9 @@
+def UpdateString(string, replacing_variable, index):
+      string = list(string)
+      index = int(index)
+      for i in string:
+            if string[index] == i:
+                  string[index] = replacing_variable
+      updatedString = "".join(string)
+      return print("OUTPUT", updatedString)
 
